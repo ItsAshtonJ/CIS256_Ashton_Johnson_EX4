a1b734234bbd803088f3b12ec4806a73c62289ac
ItsAshtonJ <itsashtonj@gmail.com>
2026-04-21 15:10:30 -0700

    Changed test page as pytest would not work, switched it to unittest, had to change format to unittest as well

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -4,36 +4,40 @@
 
 # Part 3: Writing Tests
 
-import pytest
+import unittest
 from guess_the_word import (
-    valid_guess, select_random_word, display_word
+    valid_guess, select_random_word, display_word, answer_list_computers, answer_list_creatures
 )
 
-def test_word_from_category():
-      # Make sure the word comes from the lists
-      from guess_the_word import answer_list_computers, answer_list_creatures
+class TestGuessWord(unittest.TestCase):
+    def test_word_from_category(self):
+        # Make sure the word comes from the lists
+        word1 = select_random_word(answer_list_computers)
+        self.assertIn(word1, answer_list_computers, f"{word1} not in computer terms list")
 
-      word1 = select_random_word(answer_list_computers)
-      assert word1 in answer_list_computers, f"'{word1}' not in computer terms list"
+        word2 = select_random_word(answer_list_creatures)
+        self.assertIn(word2, answer_list_creatures, f"{word2} not in creatures list")
 
-      word2 = select_random_word(answer_list_creatures)
-      assert word2 in answer_list_creatures, f"'{word2}' not in creatures list"
+    def test_contains_letter(self):
+        #Tests if c would be a valid guess
+        guessed_letters = set('ab')
+        self.assertTrue(valid_guess("c", guessed_letters))
+    
+    def test_invalid_guess(self):
+        #Some invalid options to make sure false would work
+        self.assertFalse(valid_guess("abc", set()))
+        self.assertFalse(valid_guess("1", set()))
+    
+    def test_display_word(self):
+        #sets the variables to help guide this test
+        word = "frog"
+        user_guess = set('ro')
 
-def test_display_word():
-     word = "frog"
-     user_guess = set('ro')
+        #gives expected result, and tells itself if that result is what it shown
+        result = display_word(word, user_guess)
+        expected = "_ r o _"
+        self.assertEqual(result, expected, f"Expected {expected}, got {result}")
+ 
+if __name__ == "__main__":
+     unittest.main(verbosity=2, exit=False) #looked up verbosity explains more of unittest
 
-     result = display_word(word, user_guess)
-     expected = "_ r o g"
-     assert result == expected, f"Expected '{expected}', got '{result}'"
-
-def test_valid_guess():    
-    guessed_letters = set('ab')
-
-    #Test some valid or true statements
-    assert valid_guess('c', guessed_letters) == True #allows valid guess
-    assert valid_guess('Z', set()) == True #allow uppercase
-
-    #Test the false statements
-    assert valid_guess('abc', set()) == False #dont allow multiple letters
-    assert valid_guess('1', set()) == False  #dont allow numbers
